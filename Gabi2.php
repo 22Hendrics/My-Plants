dc6e7eb28ebde82bda764cce9fcede83c8837a67
22Hendrics <72902948+22Hendrics@users.noreply.github.com>
2020-11-21 16:58:11 +0800

    Create Gabi2.php

---
+++ Gabi2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Plant 1</title>
+	  <meta charset="UTF-8">
+	  <link rel="stylesheet" type="text/css" href="Style2.css" />
+<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+   </head>
+   
+   <body>
+    
+	<div id="header">
+	  <center><h1>Gabi2 </h1></center>
+	  <h2>It is common to sell it grows fast and its pretty </h2> 
+	</div>
+	  
+	  <center>
+		<img src="gabi2.jpg" alt="gabi2 sa dalan" width="600" height="400">
+		<br />
+		
+	<div id="Go">
+		<a href="Plantito.php" class="active"> Go back home </a>
+	</div>
+	  </center>
+	 
+		
+		
+		
+   </body>
+</html>
