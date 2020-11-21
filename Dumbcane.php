a38d8e27250beca1130459911ae1e6882f63d674
22Hendrics <72902948+22Hendrics@users.noreply.github.com>
2020-11-21 16:58:52 +0800

    Create Dumbcane.php

---
+++ Dumbcane.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Plant 2</title>
+	  <meta charset="UTF-8">
+	  <link rel="stylesheet" type="text/css" href="Style2.css" />
+<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+   </head>
+   
+   <body>
+    
+	<div id="header">
+	  <center><h1>Dumb cane </h1></center>
+	  <h2>The stem of this plant is itchy and poisonous and it acts as defense mechanism to avoid danger </h2>
+	</div>
+	  
+	  <center>
+		<img src="dumbcane-2.jpg" alt="Poisonous Plant" width="600" height="400">
+		<br />
+		
+	<div id="Go">
+		<a href="Plantito.php" class="active"> Go back home </a>
+	</div>
+	  </center>
+	 
+		
+		
+		
+   </body>
+</html>
