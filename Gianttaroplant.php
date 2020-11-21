b1e8f9093422e1dce701438b9631fe2677e56463
22Hendrics <72902948+22Hendrics@users.noreply.github.com>
2020-11-21 16:59:40 +0800

    Create Gianttaroplant.php

---
+++ Gianttaroplant.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Plant 3</title>
+	  <meta charset="UTF-8">
+	  <link rel="stylesheet" type="text/css" href="Style2.css" />
+<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+   </head>
+   
+   <body>
+    
+	<div id="header">
+	  <center><h1>Badyang(Giant Taro plant) </h1></center>
+	  <h2>It cost 50k if its fully grown but it is banned to sell in publicly </h2>
+	</div>
+	  
+	  <center>
+		<img src="Gianttaro-plant.jpg" alt="Poisonous Plant" width="600" height="400">
+		<br />
+		
+	<div id="Go">
+		<a href="Plantito.php" class="active"> Go back home </a>
+	</div>
+	  </center>
+	 
+		
+		
+		
+   </body>
+</html>
