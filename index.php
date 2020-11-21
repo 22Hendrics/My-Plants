199d5807e5c720b0ffc10152ac41bd8525184f58
22Hendrics <72902948+22Hendrics@users.noreply.github.com>
2020-11-21 16:56:17 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Plantito</title>
+	  <meta charset="UTF-8">
+	  <link rel="stylesheet" type="text/css" href="Style2.css" />
+<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+   </head>
+   
+   <body>
+    
+	<div id="header">
+	 <h1>My Plants </h1>
+	</div>
+	  
+	  
+		<div class="outer-grid">
+		<div class="inner-grid">
+			
+			<a href="Gabi2.php" >
+				<img src="plants/gabi2.jpg" alt="gabi2 sa dalan" width="250" height="150" id="">
+			</a>
+	  
+			<a  href="Dumbcane.php">
+				<img src="dumbcane-2.jpg" alt="itchy and poisonous plant" width="250" height="150">
+			</a>
+			
+			<a href="Gianttaroplant.php">
+				<img src="Gianttaro-plant.jpg" alt="bawal e barter plant" width="250" height="150">
+			</a>
+		</div>
+		
+		<div class="inner-grid">
+			<a href="Gabi2.php">
+				<img src="gabi2.jpg" alt="gabi2 sa dalan" width="250" height="150">
+			</a>
+	  
+			<a href="Dumbcane.php">
+				<img src="dumbcane-2.jpg" alt="itchy and poisonous plant" width="250" height="150">
+			</a>
+			
+			<a href="Gianttaroplant.php">
+				<img src="pGianttaro-plant.jpg" alt="bawal e barter plant" width="250" height="150">
+			</a>
+		</div>
+		
+		<div class="inner-grid">
+			<a href="Gabi2.php">
+				<img src="gabi2.jpg" alt="gabi2 sa dalan" width="250" height="150">
+			</a>
+	  
+			<a href="Dumbcane.php">
+				<img src="dumbcane-2.jpg" alt="itchy and poisonous plant" width="250" height="150">
+			</a>
+			
+			<a href="Gianttaroplant.php">
+				<img src="Gianttaro-plant.jpg" alt="bawal e barter plant" width="250" height="150">
+			</a>
+		</div>
+		</div>
+		
+   </body>
+</html>
